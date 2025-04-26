348261cf4ff8f256f4a3cd895d8d3a01df4cb7eb
Tyler <ty.ho1126@gmail.com>
2025-04-26 22:41:46 +0800

    fix: image missing.

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "./",
-  // base: '/personal_web/',
+  base: "/personal_web/",
   plugins: [svgr(), react()],
 });
