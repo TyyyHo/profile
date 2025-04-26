cc5b53792624dac5554355260d5f49ef4dec1b3c
Tyler <ty.ho1126@gmail.com>
2025-04-26 22:04:31 +0800

    feat: add marquee.

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -4,16 +4,19 @@ import "./home_mobile.scss";
 
 import Planet from "./planet/planet";
 import Fading from "../title/fading/fading";
+import InfiniteMarquee from "../infinite-marquee";
 
 const Home = () => {
   return (
     <div id="homeContainer">
       <Planet />
-      <div className="paragraph">
+      <div className="paragraph" style={{ pointerEvents: "none" }}>
         <Fading text={"Welcome!"} />
         <h2 className="trigger">My name is Tyler.</h2>
         <p>Front-end Developer</p>
       </div>
+
+      <InfiniteMarquee />
     </div>
   );
 };
---
+++ src/components/infinite-marquee/index.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Marquee from "react-fast-marquee";
+import { TechStackList } from "../../lib/constant";
+
+const speed = 80;
+const direction = "left";
+
+export default function InfiniteMarquee() {
+  return (
+    <div id="infiniteMarquee">
+      <Marquee speed={speed} direction={direction}>
+        {TechStackList.map((item) => (
+          <div key={item.id}>
+            <img src={item.imageUrl} alt={item.name} />
+          </div>
+        ))}
+      </Marquee>
+    </div>
+  );
+}
